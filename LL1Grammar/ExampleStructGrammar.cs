3a60f5b757387049336509a4ef27abdfa4c8f81c
Artem Frolov <frolovart161@gmail.com>
2020-08-14 17:22:00 +0700

    "@" modifier support added

--- LL1Grammar/ExampleStructGrammar.cs
+++ LL1Grammar/ExampleStructGrammar.cs
@@ -16,7 +16,7 @@ namespace LL1GrammarUI
                       "~-> ~|\\t~|\\n~|$" + Environment.NewLine +
                       "Title->Mod ~struct ~StructName|~struct ~StructName" + Environment.NewLine +
                       "Mod->private|public|internal|protected" + Environment.NewLine +
-                      "StructName->a-z<A1>NxtName~<A4><A2>|A-Z<A1>NxtName~<A4><A2>|0-9<A1>NxtName~<A4><A2>|_<A1>NxtName~<A4><A2>" + Environment.NewLine +
+                      "StructName->a-z<A1>NxtName~<A4><A2>|A-Z<A1>NxtName~<A4><A2>|0-9<A1>NxtName~<A4><A2>|_<A1>NxtName~<A4><A2>|@<A1>NxtName~<A4><A2>" + Environment.NewLine +
                       "NxtName->a-z<A1>NxtName|A-Z<A1>NxtName|0-9<A1>NxtName|_<A1>NxtName|$" + Environment.NewLine +
                       "Body->{~Elements~}<A0>~SplitterStruct" + Environment.NewLine +
                       "SplitterStruct->;|$" + Environment.NewLine +
@@ -30,7 +30,7 @@ namespace LL1GrammarUI
                       "DataType->Alias|System.FullTypeName|FullTypeName" + Environment.NewLine +
                       "Alias->bool<A5>|byte<A5>|sbyte<A5>|char<A5>|decimal<A5>|double<A5>|float<A5>|int<A5>|uint<A5>|long<A5>|ulong<A5>|short<A5>|ushort<A5>|object<A5>|string<A5>|dynamic<A5>" + Environment.NewLine +
                       "FullTypeName->Boolean<A5>|Byte<A5>|SByte<A5>|Char<A5>|Decimal<A5>|Double<A5>|Single<A5>|Int32<A5>|UInt32<A5>|Int64<A5>|UInt64<A5>|Int16<A5>|UInt16<A5>|Object<A5>|String<A5>" + Environment.NewLine +
-                      "FName->a-z<A1>NxtName|A-Z<A1>NxtName|0-9<A1>NxtName|_<A1>NxtName";
+                      "FName->a-z<A1>NxtName|A-Z<A1>NxtName|0-9<A1>NxtName|_<A1>NxtName|@<A1>NxtName";
 
             data = "public struct exStruct" + Environment.NewLine +
                    "{" + Environment.NewLine +
